798c6d08b06f090277a28cb5ce0921fa6f1aff23
leijie_local <leijie.zhang.cn@gmail.com>
2020-01-05 12:08:28 +1100

    change dataset form

--- lei_test.py
+++ lei_test.py
@@ -2,10 +2,10 @@ import torch
 from utils import dataset_parse
 
 dataset_list = ['abalone', 'airfoil', 'bikesharing',
-                             'blog', 'bodyfat', 'CASP', 'CCPP', 'eegDual_sub1_format',
-                             'housing', 'HRSS_anomalous_optimized', 'HRSS_anomalous_standard',
-                             'kc_house', 'motor_temperature', 'quake', 'skills',
-                             'strength', 'telemonitoring', 'yacht']
+                'blog', 'bodyfat', 'CASP', 'CCPP', 'eegDual_sub1_format',
+                'housing', 'HRSS_anomalous_optimized', 'HRSS_anomalous_standard',
+                'kc_house', 'motor_temperature', 'quake', 'skills',
+                'statlib_calhousing', 'strength', 'telemonitoring', 'yacht']
 for i in torch.arange(len(dataset_list)):
     dataset_name = dataset_list[i]
 
