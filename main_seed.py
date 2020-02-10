99d596d0bbb08502f5c3b49a3d26ce0e91065d5c
leijie_local <leijie.zhang.cn@gmail.com>
2020-02-11 01:28:35 +1100

    finish seed main

--- main_seed.py
+++ main_seed.py
@@ -1,6 +1,6 @@
 from param_config import ParamConfig
 from loss_utils import RMSELoss, LikelyLoss
-from dfnn_run import fuzzy_net_run, svm_local, mlp_run, neuron_run, dfnn_kfolds, dfnn_ite_rules_mu
+from dfnn_run import fuzzy_net_run, mlp_run, neuron_run, dfnn_kfolds, dfnn_ite_rules_mu
 from utils import load_data, load_eeg_data
 import torch
 import os
@@ -8,6 +8,7 @@ import os
 
 # Dataset configuration
 # init the parameters
+n_channel = 12
 param_config = ParamConfig()
 param_config.config_parse('seed_config')
 
@@ -18,10 +19,15 @@ acc_c_train_arr = []
 acc_c_test_arr = []
 acc_d_train_arr = []
 acc_d_test_arr = []
+
+acc_c_train_list = []
+acc_c_test_list = []
+acc_d_train_list = []
+acc_d_test_list = []
 for i in torch.arange(len(param_config.dataset_list)):
     dataset_file = param_config.get_cur_dataset(int(i))
     # load dataset
-    train_data, test_data = load_eeg_data(dataset_file, "seed/channel12")
+    train_data, test_data = load_eeg_data(dataset_file, f"seed/channel{n_channel}")
 
     param_config.log.debug(f"=====starting on {train_data.name}=======")
     loss_fun = None
@@ -32,13 +38,27 @@ for i in torch.arange(len(param_config.dataset_list)):
         param_config.log.war(f"=====Mission: Regression=======")
         param_config.loss_fun = RMSELoss()
 
-    acc_c_train, acc_c_test, acc_d_train, acc_d_test = \
+    acc_c_train_tsr, acc_c_test_tsr, acc_d_train_tsr, acc_d_test_tsr = \
         dfnn_ite_rules_mu(15, param_config, train_data, test_data)
 
-    acc_c_train_arr.append(acc_c_train)
-    acc_c_test_arr.append(acc_c_test)
-    acc_d_train_arr.append(acc_d_train)
-    acc_d_test_arr.append(acc_d_test)
+    # save all the output
+    acc_c_train_list.append(acc_c_train_tsr)
+    acc_c_test_list.append(acc_c_test_tsr)
+    acc_d_train_list.append(acc_d_train_tsr)
+    acc_d_test_list.append(acc_d_test_tsr)
+
+    # get the best performance
+    acc_d_test_best = acc_d_test_tsr.max()
+    pos_best = torch.where(acc_d_test_best == acc_d_test_tsr)
+    acc_c_train_best = acc_c_train_tsr[pos_best[0], pos_best[0]]
+    acc_c_test_best = acc_c_test_tsr[pos_best[0], pos_best[0]]
+    acc_d_train_best = acc_d_train_tsr[pos_best[0], pos_best[0]]
+    acc_d_test_best = acc_d_test_tsr[pos_best[0], pos_best[0]]
+
+    acc_c_train_arr.append(acc_c_train_best)
+    acc_c_test_arr.append(acc_c_test_best)
+    acc_d_train_arr.append(acc_d_train_best)
+    acc_d_test_arr.append(acc_d_test_best)
 
 acc_c_train = torch.tensor(acc_c_train_arr).mean()
 acc_c_test = torch.tensor(acc_c_test_arr).mean()
@@ -53,3 +73,21 @@ param_config.log.info(f"mAp of training data on centralized method: {round(float
 param_config.log.info(f"mAp of test data on centralized method: {round(float(acc_c_test), 4)}/{round(float(acc_c_test_std), 4)}")
 param_config.log.info(f"mAp of training data on distributed method: {round(float(acc_d_train), 4)}/{round(float(acc_d_train_std), 4)}")
 param_config.log.info(f"mAp of test data on distributed method: {round(float(acc_d_test), 4)}/{round(float(acc_d_test_std), 4)}")
+
+dave_dict = dict()
+dave_dict["acc_c_train_list"] = acc_c_train_list
+dave_dict["acc_c_test_list"] = acc_c_test_list
+dave_dict["acc_d_train_list"] = acc_d_train_list
+dave_dict["acc_d_test_list"] = acc_d_test_list
+
+dave_dict["acc_c_train_arr"] = acc_c_train_arr
+dave_dict["acc_c_test_arr"] = acc_c_test_arr
+dave_dict["acc_d_train_arr"] = acc_d_train_arr
+dave_dict["acc_d_test_arr"] = acc_d_test_arr
+
+data_save_dir = f"./results/seed/"
+
+if not os.path.exists(data_save_dir):
+    os.makedirs(data_save_dir)
+data_save_file = f"{data_save_dir}/channel{n_channel}.pt"
+torch.save(dave_dict, data_save_file)
